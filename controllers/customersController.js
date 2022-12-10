f30b5e1dc594ac1b36482966dd00aeedfba7d575
BrVecchi <bno.vecchi@gmail.com>
2022-12-09 23:57:50 -0300

    feat: find customer by id and create customer

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -2,9 +2,41 @@ import { connectionDB } from "../database/db.js"
 
 export async function findAllCustomers(req, res) {
     try {
-    const customers = await connectionDB.query("SELECT * FROM customers;")
-        res.status(200).send(customers.rows)
+    const customers = (await connectionDB.query("SELECT * FROM customers;")).rows
+        res.status(200).send(customers)
     } catch (error) {
         res.sendStatus(error.message)
     }
 }
+
+export async function findCustmerById(req, res) {
+    const {id} = req.params
+    try {
+    const customer = (await connectionDB.query("SELECT * FROM customers WHERE id=$1;", [id])).rows
+    if (customer.length === 0) {
+        res.sendStatus(404)
+    }
+    res.status(200).send(customer)
+    } catch (error) {
+        res.sendStatus(error.message)
+    }
+}
+
+export async function createCustomer(req, res) {
+    const {name, phone, cpf, birthday} = req.body;
+
+    try {
+        const customers = (await connectionDB.query("SELECT * FROM customers;")).rows
+        const customersCPF = customers.map(customer => customer.cpf)
+        if (customersCPF.includes(cpf)) {
+            res.sendStatus(409)
+            return
+        }
+
+        await connectionDB.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);', [name, phone, cpf, birthday])
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(error.message)
+    }
+}
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { findAllCategories, createCategory } from "../controllers/categoriesController.js";
-import { findAllCustomers } from "../controllers/customersController.js";
+import { createCustomer, findAllCustomers, findCustmerById } from "../controllers/customersController.js";
 import { createGame, findAllGames } from "../controllers/gamesController.js";
 import { findAllRentals } from "../controllers/rentalsController.js";
 
@@ -10,6 +10,8 @@ router.get("/rentals", findAllRentals);
 router.get("/games", findAllGames);
 router.post("/games", createGame);
 router.get("/customers", findAllCustomers);
+router.get("/customers/:id", findCustmerById);
+router.post("/customers", createCustomer);
 router.get("/categories", findAllCategories);
 router.post("/categories", createCategory);
 
